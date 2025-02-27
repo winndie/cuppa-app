1731665c12f3dff3ff3c75e3a26ea3bcb2802cfe
Winndie Fan <winndie@gmail.com>
2025-02-27 18:09:12 +0000

    git ignore

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App:React.FC = () => {
   },[dispatch])
 
   return (
-    !!drinks?
+    drinks?
     <div className="container">
     {cooking && !!recipes ?
       <div className='readout'>
